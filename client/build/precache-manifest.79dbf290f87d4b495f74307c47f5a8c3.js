45c7e53133fa6dba073415fd792af91a4a1a4647
Preksha2002 <97279851+Preksha2002@users.noreply.github.com>
2023-04-27 17:56:24 +0530

    Add files via upload

---
+++ client/build/precache-manifest.79dbf290f87d4b495f74307c47f5a8c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff79aa095c4b54340edf14bff60c247",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b055fc0d7b467cee61a",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6b055fc0d7b467cee61a",
+    "url": "/static/js/2.37dd33ab.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.37dd33ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887559166e6c5435f57a",
+    "url": "/static/js/main.42fa6f4e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
